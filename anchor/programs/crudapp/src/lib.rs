4cef229a05c3905320fdebbd5165165521257a17
Prashant <prashantco111@gmail.com>
2024-12-19 12:59:47 +0530

    Added base PDA for Journal Entry

--- anchor/programs/crudapp/src/lib.rs
+++ anchor/programs/crudapp/src/lib.rs
@@ -4,67 +4,47 @@ use anchor_lang::prelude::*;
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
+const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
 #[program]
 pub mod crudapp {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseCrudapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.crudapp.count = ctx.accounts.crudapp.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.crudapp.count = ctx.accounts.crudapp.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeCrudapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.crudapp.count = value.clone();
-    Ok(())
-  }
+    pub fn create_general_entry(
+        ctx: Context<CreateEntry>,
+        title: String,
+        message: String,
+    ) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct InitializeCrudapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+#[instruction(title: String)]
+pub struct CreateEntry<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
 
-  #[account(
-  init,
-  space = 8 + Crudapp::INIT_SPACE,
-  payer = payer
-  )]
-  pub crudapp: Account<'info, Crudapp>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseCrudapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+    #[account(
+        init,
+        seeds = [owner.key().as_ref(), title.as_bytes() ],
+        bump,
+        space = ANCHOR_DISCRIMINATOR_SIZE + JournalEntryState::INIT_SPACE,
+        payer = owner
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
 
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub crudapp: Account<'info, Crudapp>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub crudapp: Account<'info, Crudapp>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Crudapp {
-  count: u8,
+pub struct JournalEntryState {
+    pub owner: Pubkey,
+
+    #[max_len(50)]
+    pub title: String,
+
+    #[max_len(500)]
+    pub message: String,
 }
