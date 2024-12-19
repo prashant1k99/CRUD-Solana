83334b95dfe71df96fb5b87ad0f4a18307ca3a69
Prashant <prashantco111@gmail.com>
2024-12-19 13:22:51 +0530

    Completed Smart Contract for Journal Entry

--- anchor/programs/crudapp/src/lib.rs
+++ anchor/programs/crudapp/src/lib.rs
@@ -10,18 +10,50 @@ const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 pub mod crudapp {
     use super::*;
 
-    pub fn create_general_entry(
-        ctx: Context<CreateEntry>,
+    pub fn create_journal_entry(
+        ctx: Context<CreateJournalEntry>,
         title: String,
         message: String,
     ) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.title = title;
+        journal_entry.message = message;
+
+        Ok(())
+    }
+
+    pub fn update_journal_entry(
+        ctx: Context<UpdateJournalEntry>,
+        _title: String,
+        message: String,
+    ) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+
+        journal_entry.message = message;
+
+        Ok(())
+    }
+
+    pub fn delete_journal_entry(_ctx: Context<DeleteJournalEntry>, _title: String) -> Result<()> {
         Ok(())
     }
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState {
+    pub owner: Pubkey,
+
+    #[max_len(50)]
+    pub title: String,
+
+    #[max_len(500)]
+    pub message: String,
+}
+
 #[derive(Accounts)]
 #[instruction(title: String)]
-pub struct CreateEntry<'info> {
+pub struct CreateJournalEntry<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
 
@@ -37,14 +69,38 @@ pub struct CreateEntry<'info> {
     pub system_program: Program<'info, System>,
 }
 
-#[account]
-#[derive(InitSpace)]
-pub struct JournalEntryState {
-    pub owner: Pubkey,
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct UpdateJournalEntry<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
 
-    #[max_len(50)]
-    pub title: String,
+    #[account(
+        mut,
+        seeds = [owner.key().as_ref(), title.as_bytes() ],
+        bump, // For readjusting the rent payment
+        realloc = ANCHOR_DISCRIMINATOR_SIZE + JournalEntryState::INIT_SPACE,
+        realloc::payer = owner,
+        realloc::zero = true,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
 
-    #[max_len(500)]
-    pub message: String,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteJournalEntry<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [owner.key().as_ref(), title.as_bytes()],
+        bump, // For readjusting the rent payment
+        close = owner, // It only allows it to close if the owner is the creator
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+
+    pub system_program: Program<'info, System>,
 }
